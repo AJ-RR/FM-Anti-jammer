6a75106fadbbbf96d8d521c7895bf1b538395627
AJ-RR <ranganathajay@gmail.com>
2019-11-25 12:52:26 +0530

    added hamming window

--- fm_receiver.m
+++ fm_receiver.m
@@ -1,12 +1,12 @@
 function demodulated_sig = fm_receiver(fm_signal,fc,fs,freq_dev)
     hop_sequence = [1,2,3,4,5];  
     %hop_sequence = 1:1/50:5;
-    hop_duration = 2*fs;
+    hop_duration = 1*fs;
     demodulated_sig = [];
     m_length = length(fm_signal);
     %Demodulate sequentially
     for i = hop_sequence
-        if m_length < hop_duration
+        if m_length <= hop_duration
             demodulated = fmdemod(fm_signal((i-1)*hop_duration:length(fm_signal)),i*fc, fs, freq_dev);
             %demodulated = lowpass(demodulated, 10000, fs);
             demodulated_sig = [demodulated_sig,demodulated,zeros(hop_duration - length(fm_signal))];
@@ -24,7 +24,7 @@ function demodulated_sig = fm_receiver(fm_signal,fc,fs,freq_dev)
             end
         end
     end
-     b = 1;
-     a = [1 -0.95];
-     demodulated_sig = filter(b, a, demodulated_sig);
+%      b = 1;
+%      a = [1 -0.95];
+%      demodulated_sig = filter(b, a, demodulated_sig);
 end
--- fm_transmitter.m
+++ fm_transmitter.m
@@ -1,27 +1,42 @@
 function modulated_sig = fm_transmitter(message,Fc,Fs,freq_dev)
   hop_sequence = [1,2,3,4,5];
-  hop_duration = 2*Fs;
+  hop_duration = 1*Fs;
+  overlap  = 100;
   modulated_sig = [];
   m_length = length(message);
-   b = [1 -0.95];
-   a = 1;
-   message = filter(b,a,message);
+%   b = [1 -0.95];
+%   a = 1;
+%   message = filter(b,a,message);
   %Modulate sequentially
   for i = hop_sequence
     if m_length <= hop_duration
-          modulated = fmmod(message((i-1)*hop_duration : length(message)),i*Fc, Fs, freq_dev);
+
+          modulated = fmmod(message((i-1)*hop_duration - overlap : length(message)),i*Fc, Fs, freq_dev);
           modulated_sig = [modulated_sig,modulated, zeros(hop_duration - length(message))];
       else
         if(i == 1)
-            modulated = fmmod(message(1:hop_duration),i*Fc,Fs,freq_dev);
+            w = hamming(length(message(1:hop_duration + overlap)),'periodic');
+            modulated = fmmod(message(1:hop_duration + overlap).*(transpose(w)),i*Fc,Fs,freq_dev);
             modulated_sig = [modulated_sig,modulated];
             m_length = m_length - hop_duration;
         else
-            modulated = fmmod(message((i - 1)*hop_duration : i*hop_duration), i*Fc, Fs, freq_dev);
-%             modulated = fmmod(message((i - 1)*hop_duration : i*hop_duration), i, Fs, freq_dev);
+%             modulated = fmmod(message((i - 1)*hop_duration : i*hop_duration), i*Fc, Fs, freq_dev);
+            w = hamming(length(message((i - 1)*hop_duration - overlap: i*hop_duration + overlap)),'periodic');
+            modulated = fmmod(message((i - 1)*hop_duration - overlap: i*hop_duration + overlap).*(transpose(w)) , i*Fc, Fs, freq_dev);
             modulated_sig = [modulated_sig,modulated];
             m_length = m_length - hop_duration;
         end
       end
   end
+%   Wo = 4000/(Fs/2);  BW = Wo/20;
+%   [b,a] = iirnotch(Wo,BW);
+%   modulated_sig = filter(b,a,modulated_sig);
+%   
+%   Wo = 8000/(Fs/2);  BW = Wo/20;
+%   [b,a] = iirnotch(Wo,BW);
+%   modulated_sig = filter(b,a,modulated_sig);
+%   
+%   Wo = 12000/(Fs/2);  BW = Wo/20;
+%   [b,a] = iirnotch(Wo,BW);
+%   modulated_sig = filter(b,a,modulated_sig);
   end  
